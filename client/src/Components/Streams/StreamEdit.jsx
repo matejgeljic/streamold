32dc94007bc2f368f420a18705d3336b9c2baebd
Matej <matejgeljic98@gmail.com>
2019-09-26 23:56:29 +0200

    json server, stream edit

--- client/src/Components/Streams/StreamEdit.jsx
+++ client/src/Components/Streams/StreamEdit.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import { fetchStream, editStream } from '../../Actions';
+import StreamForm from './StreamForm';
 
-const StreamEdit = () => {
-    return (
-        <div>StreamEdit</div>
-    );
+class StreamEdit extends React.Component {
+    
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id, formValues);
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm 
+                    initialValues={_.pick(this.props.stream, 'title', 'description')}
+                    onSubmit={this.onSubmit} 
+                />
+            </div>
+        );
+    }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    }
+}
+
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
